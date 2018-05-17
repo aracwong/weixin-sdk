f741a8dc1fcb6dda70a1d35168ba90910d9112f4
aracwong <aracwong@163.com>
2018-05-17 21:08:04 +0800

    添加核心处理器

---
+++ README.md
@@ -0,0 +1,13 @@
+微信 java sdk 
+
+```
+修订记录: 
+
+- 2018/05
+
+```
+
+核心处理器：WxHandlerDispatcher
+
+
+
--- src/main/java/com/github/aracwong/weixin/framework/core/WxHandlerDispatcher.java
+++ src/main/java/com/github/aracwong/weixin/framework/core/WxHandlerDispatcher.java
@@ -1,8 +1,19 @@
 package com.github.aracwong.weixin.framework.core;
 
 import com.github.aracwong.weixin.framework.context.WxAppContext;
+import com.google.common.base.Strings;
+import com.thoughtworks.xstream.XStream;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.DigestUtils;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,15 +23,81 @@ import java.util.List;
  * @version 1.0.0
  */
 @Slf4j
-public class WxHandlerDispatcher {
+public class WxHandlerDispatcher extends HttpServlet {
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String reqMethod = req.getMethod();
+        String timestamp = req.getParameter("timestamp");
+        String nonce = req.getParameter("nonce");
+        String signature = req.getParameter("signature");
+
+        try {
+            /** 微信认证请求处理 */
+            if ("GET".equals(reqMethod)) {
+                String echoStr = req.getParameter("echostr");
+                List<String> list = new ArrayList<>();
+                list.add(timestamp);
+                list.add(nonce);
+
+                // 获取 token TODO
+                StringBuffer sb = new StringBuffer();
+                list.forEach(str -> sb.append(str));
+
+                String encryptStr = DigestUtils.sha1Hex(sb.toString());
+                if (encryptStr.equals(signature)) {
+                    log.info("========== 认证成功！");
+                    resp.getOutputStream().print(echoStr);
+                    resp.getOutputStream().flush();
+                } else {
+                    log.info("========== 认证失败！");
+                }
+
+            }
+
+            /** 微信数据请求处理 */
+            else if ("POST".equals(reqMethod)) {
+                String openid = req.getParameter("openid");
+                ByteArrayOutputStream result = new ByteArrayOutputStream();
+                InputStream reqIs = req.getInputStream();
+                byte[] buff = new byte[1024];
+                int index;
+                while ((index = reqIs.read(buff)) != -1) {
+                    result.write(buff, 0, index);
+                }
+
+                reqIs.close();
+
+                String reqBody = result.toString();
+                if (!Strings.isNullOrEmpty(reqBody)) {
+                    WxDefaultRequest wxRequest = new WxDefaultRequest(reqBody);
+                    WxResponse wxResponse = new WxDefaultResponse(wxRequest);
+
+                    this.doDispatch(wxRequest, wxResponse);
+
+                    String KEY_RESPONSE_BODY = "body";
+                    Object responseBody = wxResponse.getAttribute(KEY_RESPONSE_BODY);
+                    if (null != wxResponse.getAttribute(KEY_RESPONSE_BODY)) {
+                        XStream xStream = new XStream();
+                        xStream.autodetectAnnotations(true);
+                        String xml = xStream.toXML(responseBody);
+                        log.info("响应报文：\r\n{}", xml);
+                        resp.getOutputStream().print(xml);
+                        resp.getOutputStream().flush();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("处理微信请求异常!", e);
+        }
+    }
 
     /**
      * 微信消息核心处理器
      *
-     * @param request
-     *        请求对象
-     * @param response
-     *        响应对象
+     * @param request  请求对象
+     * @param response 响应对象
      */
     public void doDispatch(WxRequest request, WxResponse response) {
         WxRequestFilterChain wxHandlerChain = null;
