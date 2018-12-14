91ed8582e595288730e32edbb6089aec2b72a126
aracwong <aracwong@163.com>
2018-12-14 11:24:22 +0800

    重构

--- src/main/java/com/github/aracwong/wx/mp/framework/core/WxMsgHandlerRegistration.java
+++ src/main/java/com/github/aracwong/wx/mp/framework/core/WxMsgHandlerRegistration.java
@@ -1,17 +1,6 @@
 package com.github.aracwong.wx.mp.framework.core;
 
-import com.github.aracwong.wx.mp.framework.handler.event.*;
-import com.github.aracwong.wx.mp.framework.handler.file.DefaultWxFileRequestMsgHandler;
-import com.github.aracwong.wx.mp.framework.handler.image.DefaultWxImageRequestMsgHandler;
-import com.github.aracwong.wx.mp.framework.handler.link.DefaultWxLinkRequestMsgHandler;
-import com.github.aracwong.wx.mp.framework.handler.location.DefaultWxLocationRequestMsgHandler;
-import com.github.aracwong.wx.mp.framework.handler.text.DefaultWxTextRequestMsgHandler;
-import com.github.aracwong.wx.mp.framework.handler.video.DefaultWxVideoRequestMsgHandler;
-import com.github.aracwong.wx.mp.framework.handler.voice.DefaultWxVoiceRequestMsgHandler;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -20,38 +9,29 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class WxMsgHandlerRegistration {
 
-    public static Map<String, WxDelegateRequestMsgFilter> handlerFiltersMapping = new ConcurrentHashMap<>();
+    public static Map<String, WxMsgHandler> handlerFiltersMapping = new ConcurrentHashMap<>();
 
     public WxMsgHandlerRegistration() {
         init();
     }
 
     private void init() {
-        registerHandler(new DefaultWxTextRequestMsgHandler());
-        registerHandler(new DefaultWxImageRequestMsgHandler());
-        registerHandler(new DefaultWxVoiceRequestMsgHandler());
-        registerHandler(new DefaultWxVideoRequestMsgHandler());
-        registerHandler(new DefaultWxLinkRequestMsgHandler());
-        registerHandler(new DefaultWxLocationRequestMsgHandler());
-        registerHandler(new DefaultWxFileRequestMsgHandler());
-
-        registerHandler(new DefaultWxFollowEventMsgHandler());
-        registerHandler(new DefaultWxQrCodeEventMsgHandler());
-        registerHandler(new DefaultWxLocationEventMsgHandler());
-        registerHandler(new DefaultWxMenuEventMsgHandler());
-        registerHandler(new DefaultWxScanCodeEventMsgHandler());
-        registerHandler(new DefaultWxPicPhotoEventMsgHandler());
-        registerHandler(new DefaultWxLocationSelectEventMsgHandler());
+        ServiceLoader<WxMsgHandler> serviceLoader = ServiceLoader.load(WxMsgHandler.class);
+        Iterator<WxMsgHandler> it = serviceLoader.iterator();
+        while (it != null && it.hasNext()) {
+            WxMsgHandler handler = it.next();
+            this.registerHandler(handler);
+        }
     }
 
-    public WxMsgHandlerRegistration registerHandler(WxDelegateRequestMsgFilter handler) {
+    public WxMsgHandlerRegistration registerHandler(WxMsgHandler handler) {
         handlerFiltersMapping.put(handler.getHandlerKey(), handler);
         return this;
     }
 
     List<WxRequestFilter> getHandlerFilters() {
         List<WxRequestFilter> handlerFilters = new ArrayList<>();
-        handlerFiltersMapping.forEach((key, value) -> handlerFilters.add(value));
+        handlerFiltersMapping.forEach((key, value) -> handlerFilters.add((WxRequestFilter)value));
         return handlerFilters;
     }
 
